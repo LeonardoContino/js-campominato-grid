a9f84da09b1fa83732e179ae9ed37db0fb1449ce
LeonardoContino <leoconti2000@hotmail.it>
2022-12-16 15:09:07 +0100

    mod css

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("js ok");
+
+/*
+MILESTONE 1
+Prepariamo l'HTML ed il CSS per ottenere il risultato grafico
+ che vediamo nell'immagine allegata.
+MILESTONE 2
+Rimuoviamo le celle che abbiamo inserito nell'HTML
+ in modo da generarle tramite JS. Al click del bottone play,
+  vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+MILESTONE 3
+In ogni cella, deve comparire il numero corrispondente,
+ in ordine da 1 a 100;
+#MILESTONE 4
+Al click sulla cella, stampiamo il numero della cella cliccata in console,
+ poi coloriamo la cella d'azzurro!
+*/
+
+const btn = document.getElementById("btn");
+const box = document.getElementById("map");
+
+btn.addEventListener("click", function () {});
